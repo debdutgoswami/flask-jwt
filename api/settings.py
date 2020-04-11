2c74b637c2ecb9df8d0877d8c27a0fc263f7be3e
Debdut Goswami <debdutgoswami@gmail.com>
2020-04-12 03:21:01 +0530

    ADD settings and config

---
+++ api/settings.py
@@ -0,0 +1,8 @@
+import os
+from os import environ
+
+SECRET_KEY = environ.get('SECRET_KEY')
+API_KEY = environ.get('API_KEY')
+SQLALCHEMY_DATABASE_URI='sqlite:///Database.db'
+#SQLALCHEMY_DATABASE_URI=environ.get('SQLALCHEMY_DATABASE_URI')
+SQLALCHEMY_TRACK_MODIFICATIONS=environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
